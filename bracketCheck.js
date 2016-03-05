57c5ad3766c63f4a90fed5e1fc13dc85533fbcb9
k8e <k8eSchaffer@gmail.com>
2016-03-05 01:55:50 -0600

    Added javascript function from Hackerrank challenge

---
+++ bracketCheck.js
@@ -0,0 +1,35 @@
+function bracketTest(brackStrings) {
+// Returns array of YES/NO values, indicating whether or not the
+// inputted strings of brackets are closed
+
+  var bracketMatch = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  };
+  var results = [];
+  // Go through all the bracket strings
+  brackStrings.forEach(function(brackStr) {
+    var stack = [];
+    var closed = true;
+    var brackets = brackStr.split('');
+    for (var i = 0; i < str.length; i++) {
+      if (brackets[i] == '{' || brackets[i] == '[' || brackets[i] == '(') {
+        stack.push(brackets[i]);
+      } else if (brackets[i] == '}' || brackets[i] == ']' || brackets[i] == ')') {
+        var openBracket = stack.pop();
+        // Test for matching closing bracket to most recently opened bracket
+        if (!(bracketMatch[openBracket] == brackets[i])) {
+          closed = false;
+        }
+      }
+    }
+    // Put together results for this bracket string
+    if (closed && stack.length == 0) {
+      results.push("YES");
+    } else {
+      results.push("NO");
+    }
+  });
+  return results;
+}
