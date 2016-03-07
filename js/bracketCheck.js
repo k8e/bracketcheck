b5c9696fcb9c5026fca7605688ca5a757d608cc1
k8e <k8eSchaffer@gmail.com>
2016-03-07 15:18:14 -0600

    Added a few comments

--- js/bracketCheck.js
+++ js/bracketCheck.js
@@ -9,12 +9,14 @@ function bracketCheck(testStrings) {
 // inputted strings of brackets are closed according to closer map
   var results = [];
 
+  // Go through all the strings in given array
   testStrings.forEach(function(str) {
-    var closed = true;  // Initially assume all brackets are closed
+    var closed = true;  // Are all brackets in the string closed?
     var currChar;       // Current character in string
     var openBracket;    // Currently open bracket
     var stack = [];     // Stack of open brackets
 
+    // Go through every character in the string
     var chars = str.split('');
     for (var pos = 0; pos < str.length; pos++) {
       currChar = chars[pos];
@@ -31,7 +33,7 @@ function bracketCheck(testStrings) {
     }
 
     // Append results for this string
-    if (closed && stack.length == 0) {
+    if (closed && stack.length == 0) {  // (The open bracket stack must be empty)
       results.push(true);
     } else {
       results.push(false);
